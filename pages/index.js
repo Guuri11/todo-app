999da19b514c5e075a342b083d24d0a69ad5ab4f
Guuri11 <sergio.gurillo11@gmail.com>
2021-03-28 13:35:21 +0200

    Todo app finished

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,70 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+
+  const [todos, setTodos] = useState([{id:0, name:"First todo", done: 0}, {id: 1, name:"Second todo", done: 0}])
+  const [input, setInput] = useState("")
+
+  const handleChange = e => {
+    setInput(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    
+    let aux_todos = todos
+
+    aux_todos.push({id: aux_todos[aux_todos.length -1].id + 1,name:input, done: 0})
+
+    setTodos(aux_todos)
+    setInput("")
+
+  }
+
+  const handleDone = id => {
+    let aux_todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.done = todo.done === 0 ? 1:0
+      }
+
+      return todo
+    })
+
+    setTodos(aux_todos)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Todo app with Next JS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h4 className={styles.title}>
+          Welcome to <a href="https://github.com/Guuri11">Todo app with Next JS!</a>
+        </h4>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div className={styles.form_todo}>
+          <input type="text" defaultValue={input} value={input} onChange={handleChange} />
+          <button onClick={handleSubmit}>Create note</button>
+        </div>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        {
+          todos.map((todo, idx) => {
+            return (
+              <div key={idx} className={styles.card}>
+                <p>{todo.name}</p>
+                <button onClick={() => handleDone(todo.id)}>{todo.done ? "Done":"Not done"}</button>
+              </div>  
+            )
+          })
+        }
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
